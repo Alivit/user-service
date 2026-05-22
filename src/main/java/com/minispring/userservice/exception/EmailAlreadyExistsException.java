a03d253a63929d9151caec7f378af3b9dbab7955
Alivit <vltal241201@gmail.com>
2026-05-22 11:41:07 +0700

    feat: add docker

--- src/main/java/com/minispring/userservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.minispring.userservice.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
